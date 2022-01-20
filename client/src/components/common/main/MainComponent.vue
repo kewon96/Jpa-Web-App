e3206528b7c5fa5824e28fc36d6a6c562951aa9e
Kewon <48837736+kewon96@users.noreply.github.com>
2022-01-20 23:30:12 +0900

    MainComponent.vue

--- client/src/components/common/main/MainComponent.vue
+++ client/src/components/common/main/MainComponent.vue
@@ -1,11 +1,29 @@
 <template>
+  <SideBar />
+
   <h1>Hello World!</h1>
+
 </template>
 
 <script setup lang="ts">
+import {onBeforeMount} from "vue";
+import {useRouter} from "vue-router";
+import http from "../../../util/http";
+import SideBar from "./SideBar.vue";
 
+
+onBeforeMount(async () => {
+  const token: string | null = sessionStorage.getItem("token");
+
+  if (!token) {
+    useRouter().push('Login');
+  } else {
+    // 토큰있으면 검증시작
+    await http.get('/checkToken', {token})
+  }
+})
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
